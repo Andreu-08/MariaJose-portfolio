2679f09232180cf2005b2226c31c0d41b4e94fb1
Andreu Orenga <andreuorenga@gmail.com>
2025-11-08 13:18:04 +0100

    feat: eliminar archivos y componentes no utilizados del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        steel: {
+          50: '#F4F7FB',
+          100: '#E8EEF6',
+          200: '#CCDBEB',
+          300: '#9FBEDA',
+          400: '#6C9CC4',
+          500: '#5389B7',
+          600: '#376592',
+          700: '#2D5277',
+          800: '#294663',
+          900: '#263C54',
+          950: '#192738'
+        }
+      }
+    },
   },
   plugins: [],
 }
